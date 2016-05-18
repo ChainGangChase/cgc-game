90896f33c7fbc58ce8210c39e6aa75c08800463f
Joseph Pietruch <percipient24@gmail.com>
2016-05-17 23:03:23 -0400

    First Gradle/libGDX

---
+++ android/src/com/percipient24/cgc/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.percipient24.cgc;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.percipient24.cgc.ChainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ChainGame(), config);
+	}
+}
